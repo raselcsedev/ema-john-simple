251272fd9cd0409a9f9b492effbf2b0187dc3f1e
raselcsedev <raselcse.dev@gmail.com>
2022-03-31 07:27:03 +0600

    initial commit

---
+++ src/components/Shop/Shop.js
@@ -0,0 +1,66 @@
+import React, { useEffect, useState } from 'react';
+import { addToDb, getStoredCart } from '../../utilities/fakedb';
+import Cart from '../Cart/Cart';
+import Product from '../Product/Product';
+import './Shop.css'
+
+const Shop = () => {
+    const [products, setProducts] = useState([]);
+
+    const [cart, setCart] = useState([]);
+
+    useEffect(()=>{
+        fetch('products.json')
+        .then(res=>res.json())
+        .then(data=>setProducts(data))
+    },[]);
+    useEffect( ()=>{
+        const storedCart = getStoredCart();
+        const savedCart = [];
+        for(const id in storedCart){
+            const addedProduct = products.find(product => product.id === id);
+            if(addedProduct){
+                const quantity = storedCart[id];
+                addedProduct.quantity=quantity;
+                savedCart.push(addedProduct)
+            }
+        }
+        setCart(savedCart)
+    } ,[products])
+    const handleAddToCart = (product) =>{
+        let newCart  = [];
+        const exists = cart.find(selectedProduct => selectedProduct.id === product.id);
+        if(!exists){
+            product.quantity = 1;
+            newCart = [...cart, product];
+        }
+        else{
+            const rest = cart.filter(selectedProduct => selectedProduct.id !== product.id);
+            exists.quantity = exists.quantity + 1;
+            newCart = [ ...rest, exists]
+        }
+        console.log(product  );
+        // const newCart = [...cart , product];
+        setCart(newCart);
+        addToDb(product.id)
+    }
+    return (
+        <div className='shop-container'>
+            <div className="products-container">
+                {
+                    products.map(product => <Product 
+                        key={product.id}
+                        product ={product}
+                        handleAddToCart={handleAddToCart}
+                     ></Product>)
+                }
+            </div>
+            <div className="cart-container">
+                <Cart cart = {cart}></Cart>
+                
+            </div>
+        </div>
+    );
+};
+
+export default Shop;
